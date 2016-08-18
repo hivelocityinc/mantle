1ed8f367bf28d7c6f08710202b6423ade3e2301e
Ryuichi Komeda <komeda@hivelocity.co.jp>
2016-08-18 15:44:42 +0900

    Added wercker configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,6 @@ require "serverspec"
 
 set :backend, :docker
 set :docker_url, ENV["DOCKER_HOST"]
-set :docker_image, "hivelocityinc/mantle"
+set :docker_container, ENV['TARGET_CONTAINER_ID']
 
 Excon.defaults[:ssl_verify_peer] = false
