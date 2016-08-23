8d4d7c3981e189b5f725c169a447e9b91903e1be
Ryuichi Komeda <komeda@hivelocity.co.jp>
2016-08-23 20:17:17 +0900

    Modify configuration files

--- files/mysql_setup.sh
+++ files/mysql_setup.sh
@@ -2,19 +2,17 @@
 
 set -e
 
-rm -rf /var/lib/mysql
-mkdir -p /var/lib/mysql /var/run/mysqld /run/mysqld
-chown -R mysql:mysql /var/lib/mysql /var/run/mysqld /run/mysqld
-chmod -R 777 /var/run/mysqld /run/mysqld
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mkdir -p /var/lib/mysql
 
-mysql_install_db --user=mysql --datadir="/var/lib/mysql" --rpm
+    mysql_install_db --user=mysql --datadir="/var/lib/mysql" --rpm
 
-tempfile=`mktemp`
-if [ ! -f "$tempfile" ]; then
-  return 1
-fi
+    tempfile=`mktemp`
+    if [ ! -f "$tempfile" ]; then
+        return 1
+    fi
 
-cat << EOF > $tempfile
+    cat << EOF > $tempfile
   UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User='root';
   DELETE FROM mysql.user WHERE User='';
   DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
@@ -24,5 +22,6 @@ cat << EOF > $tempfile
   GRANT ALL ON ${MYSQL_DATABASE}.* to '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 EOF
 
-/usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tempfile
-rm -rf $tempfile
+    /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tempfile
+    rm -rf $tempfile
+fi
