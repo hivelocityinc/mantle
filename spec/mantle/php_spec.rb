bbbcff3ea3cbeaa526dccd0fd578a61ff031c3b5
Ryuichi Komeda <komeda@hivelocity.co.jp>
2016-11-08 20:00:37 +0900

    Added test code for composer and more

--- spec/mantle/php_spec.rb
+++ spec/mantle/php_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
-describe package('php5') do
-  it { should be_installed }
+php_packages = %w{php5 php5-common php5-fpm php5-mysqli php5-phar php5-pdo php5-pdo_mysql php5-dom php5-gd php5-xml php5-json php5-mcrypt php5-imap php5-zlib php5-opcache php5-openssl php5-imagick php5-memcached php5-redis}
+
+php_packages.each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
 end
 
 describe command('php -v') do
@@ -14,6 +18,10 @@ describe file('/etc/php5/php.ini') do
 end
 
 describe file('/etc/php5/php-fpm.conf') do
+  it { should be_file }
+end
+
+describe file('/usr/bin/composer') do
   it { should be_file }
 end
 
